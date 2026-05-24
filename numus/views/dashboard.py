6f99445b0b328f87bf807b293061543b78161732
paulsurya <paulsurya11@gmail.com>
2026-05-24 16:42:52 +0530

    feat:Added templates for dashboard, input

--- numus/views/dashboard.py
+++ numus/views/dashboard.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
+from flask.templating import render_template
 
 dashBoard_bp = Blueprint("dashBoard", __name__)
 
 
 @dashBoard_bp.route("/")
 def dashBoard():
-    return "<h>Hello, Hi!</h>"
+    return render_template("input.html")
