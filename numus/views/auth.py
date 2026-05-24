56a57ef80d70479f39ae66533426bef5e135a926
paulsurya <paulsurya11@gmail.com>
2026-05-24 19:55:45 +0530

    fix:fixed the 404 issuse with auth.py

--- numus/views/auth.py
+++ numus/views/auth.py
@@ -1,8 +1,18 @@
+from curses import flash
+
 from flask import Blueprint, render_template
 
 auth_bp = Blueprint("auth", __name__)
 
 
-@auth_bp.route("/login", methods=["POST", "GET"])
+@auth_bp.route("/login", methods=["POST", "GET"], strict_slashes=False)
 def login():
+    if methods == "POST":
+        userName = request.form.get("userName")
+        pass1 = request.form.get("password1")
+        pass2 = request.form.get("password2")
+        if pass1 != pass2:
+            print("error")
+        elif len(pass1) < 7:
+            print("error")
     return render_template("login.html")
